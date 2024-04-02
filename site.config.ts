59cdea6634b5966251f79b6e94d1f2645bde592c
Kevin Kien <kevinkien@hotmail.com>
2024-04-02 13:49:01 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -42,10 +42,10 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
+  pageUrlOverrides: {
+     '/book': '561f992e260e4717826e5e43de041386',
+     '/blog': '2bce9d34e7344f2d9860d0afc4b5671a'
+  }
   pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
